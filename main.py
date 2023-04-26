09b3612597c7b9a47da6c5701c9b79186479a1a7
Thomaz Simão <thomazsimao5@gmail.com>
2023-04-26 17:34:09 -0300

    Update
    Testes  questões 2 e 5

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-texto = "Inversão de String Teste"
+
+# Fibo 
+numero = int(input("Digite um número: "))
+
+
+fibonacci_antecessor = 0
+fibonacci_atual = 1
+
+
+while fibonacci_atual < numero:
+    fibonacci_proximo = fibonacci_antecessor + fibonacci_atual
+    fibonacci_antecessor = fibonacci_atual
+    fibonacci_atual = fibonacci_proximo
+
+if fibonacci_atual == numero:
+    print(f"O número {numero} pertence à sequência de Fibonacci!")
+else:
+ print(f"O número {numero} não pertence à sequência de Fibonacci!")
+
+ # Inversão de Strings
+
+ texto = "Inversão de String Teste"
 
 texto_invertido = texto[::-1]
 
